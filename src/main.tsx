2554ac03e628b6999685b3de0daf4f6617797685
alephbaabc <alephbaabc@gmail.com>
2025-12-29 11:32:49 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './GoldEngine'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' // This must match the filename
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
